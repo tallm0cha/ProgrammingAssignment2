0317047c0d9744dce28f8a020e59b871cab8d01c
tallm0cha <gaurav.a.singh@zsassociates.com>
2016-09-15 10:58:02 -0700

    Signed-off-by: tallm0cha <gaurav.a.singh@zsassociates.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix allows the user to store a matrix and retrieve its inversion which is calculated only once. 
+## The argument for the function is a matrix
+## The returned object list contains a series of functions and holds the original matrix and the 
+## cached inverted matrix of it.
+##    set (newMatrix) - use this to set a new matrix into this cached matrix object. 
+##                     if the matrix is the same in terms of values 
+##                     and dimensions then the inversion is not recalculated. 
+##    get - gets the stored matrix. 
+##    setinv - accepts the solve(matrix) matrix and stores it as a cached inverted matrix. 
+##    getinv - retrieves the cached inversion matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   cachedInv<-NULL
   
   set <- function(y) {
+    # check if the sames matrix is being passed
+    if(!identical(x,y)) {cachedInv<<-NULL}
     x<<-y
-    cachedInv<<-NULL
   }
   
   get<- function() x
@@ -23,11 +30,10 @@ makeCacheMatrix <- function(x = matrix()) {
   
 }
 
-
-## Write a short comment describing this function
+## cacheSolve returns the cached inverse of the matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
   cachedInv <- x$getinv()
   if(!is.null(cachedInv)) {
     message("Caching inverse...")
